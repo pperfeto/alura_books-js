e7f03b288d661c945833e52346f10fb05a6556bf
pperfeto <paulo.perfetto@gmail.com>
2022-10-03 21:25:36 -0300

    implementando metodo sort, filter e reduce

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros');
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `<div class="livro">
         <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
